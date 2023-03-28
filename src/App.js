fcf9428641c9fdfc094ec943878f8b01e9050151
Anderson Fér <andersonfer@gmail.com>
2023-03-28 18:36:32 +0100

    created a Phrase object in PHRASES array in order to simplify operation and improve readability

--- src/App.js
+++ src/App.js
@@ -3,26 +3,25 @@ import { useState, useEffect } from 'react';
 
 
 const PHRASES = [
-  ['L\'homme est un loup pour l\'homme.','Plaute'],
-['Il m\'a fait trop de bien pour en dire du mal, - Il m\'a fait trop de mal pour en dire du bien.','Pierre Corneille'],
-['Rien de grand ne s\'est accompli dans le monde sans passion.','Georg Wilhelm Friedrich Hegel'],
-['On n\'est pas sérieux, quand on a dix-sept ans.','Arthur Rimbaud'],
-['Science sans conscience n\'est que ruine de l\'âme.','François Rabelais'],
-['Il y a deux histoires : l\'histoire officielle, menteuse, qu’on enseigne, puis l\'Histoire secrète, où sont les véritables causes des événements.','Balzac'],
-['Familles! je vous hais! Foyers clos; portes refermées; possessions jalouses du bonheur.','André Gide'],
-['L\'homme n\'est pas né pour le repos.','Voltaire'],
-['Si j\'ai vu si loin, c\'est que j\'étais monté sur des épaules de géants.','Isaac Newton'],
-['Ceux qui ne peuvent se souvenir du passé sont condamnés à le répéter.','George Santayana'],
-['J\'ai le rêve qu\'un jour mes quatre enfants vivront dans une nation où ils ne seront pas jugés pour la couleur de leur peau, mais pour leur caractère.','Martin Luther King'],
-['Il n\'y a point de pires sourds que ceux qui ne veulent pas entendre.','Molière'],
-['Si ceux qui disent du mal de moi savaient exactement ce que je pense d\'eux, ils en diraient bien davantage.','Sacha Guitry'],
-['Un problème sans solution est un problème mal posé.','Albert Einstein'],
-['Le courage n\'est pas l\'absence de peur, mais la capacité de vaincre ce qui fait peur.','Nelson Mandela'],
-['Vis comme si tu devais mourir demain. Apprends comme si tu devais vivre toujours.','Mahatma Gandhi'],
-['Le souvenir est le parfum de l\'âme.','George Sand'],
-['L\'enfer c’est les autres.','Sartre'],
-['La vie sans musique est tout simplement une erreur, une torture, un exil.','Friedrich Wilhelm Nietzsche'],
-['Ce qui compte, chez un homme, ce n\'est pas la couleur de sa peau ou la texture de sa chevelure, mais la texture et la qualité de son âme.','Martin Luther King']
+  {"id":0,"content":"L'homme est un loup pour l'homme.","author":"Plaute"},
+  {"id":1,"content":"Il m'a fait trop de bien pour en dire du mal, - Il m'a fait trop de mal pour en dire du bien.","author":"Pierre Corneille"},
+  {"id":2,"content":"Rien de grand ne s'est accompli dans le monde sans passion.","author":"Georg Wilhelm Friedrich Hegel"},
+  {"id":3,"content":"On n'est pas sérieux, quand on a dix-sept ans.","author":"Arthur Rimbaud"},
+  {"id":4,"content":"Science sans conscience n'est que ruine de l'âme.","author":"François Rabelais"},
+  {"id":5,"content":"Il y a deux histoires : l'histoire officielle, menteuse, qu’on enseigne, puis l'Histoire secrète, où sont les véritables causes des événements.","author":"Balzac"},
+  {"id":6,"content":"Familles! je vous hais! Foyers clos; portes refermées; possessions jalouses du bonheur.","author":"André Gide"},
+  {"id":7,"content":"L'homme n'est pas né pour le repos.","author":"Voltaire"},
+  {"id":8,"content":"Si j'ai vu si loin, c'est que j'étais monté sur des épaules de géants.","author":"Isaac Newton"},
+  {"id":9,"content":"Ceux qui ne peuvent se souvenir du passé sont condamnés à le répéter.","author":"George Santayana"},
+  {"id":10,"content":"J'ai le rêve qu'un jour mes quatre enfants vivront dans une nation où ils ne seront pas jugés pour la couleur de leur peau, mais pour leur caractère.","author":"Martin Luther King"},
+  {"id":11,"content":"Il n'y a point de pires sourds que ceux qui ne veulent pas entendre.","author":"Molière"},
+  {"id":12,"content":"Si ceux qui disent du mal de moi savaient exactement ce que je pense d'eux, ils en diraient bien davantage.","author":"Sacha Guitry"},
+  {"id":13,"content":"Un problème sans solution est un problème mal posé.","author":"Albert Einstein"},
+  {"id":14,"content":"Le courage n'est pas l'absence de peur, mais la capacité de vaincre ce qui fait peur.","author":"Nelson Mandela"},
+  {"id":15,"content":"Vis comme si tu devais mourir demain. Apprends comme si tu devais vivre toujours.","author":"Mahatma Gandhi"},
+  {"id":16,"content":"Le souvenir est le parfum de l'âme.","author":"George Sand"},  {"id":17,"content":"L'enfer c’est les autres.","author":"Sartre"},
+  {"id":18,"content":"La vie sans musique est tout simplement une erreur, une torture, un exil.","author":"Friedrich Wilhelm Nietzsche"},
+  {"id":19,"content":"Ce qui compte, chez un homme, ce n'est pas la couleur de sa peau ou la texture de sa chevelure, mais la texture et la qualité de son âme.","author":"Martin Luther King"}
 ];
 const COLORS = [
   'LightSlateGray',
@@ -43,8 +42,9 @@ export default function App() {
 function QuoteBox() {
   const randomPhraseId = Math.floor(Math.random() * PHRASES.length);
   const randomColorId = Math.floor(Math.random() * COLORS.length);
-  const [content,author] = PHRASES[randomPhraseId];
-  const [phrase,setPhrase] = useState({content:content, author:author, id:randomPhraseId});
+
+  const randomPhrase = PHRASES[randomPhraseId];
+  const [phrase,setPhrase] = useState(randomPhrase);
   const [color,setColor] = useState('FireBrick');
 
   useEffect(() => {
@@ -63,12 +63,8 @@ function QuoteBox() {
       randomNumberForPhraseChoice = Math.floor(Math.random() * PHRASES.length);
     }
     while (randomNumberForPhraseChoice === phrase.id);
-    const [content,author] = PHRASES[randomNumberForPhraseChoice];
-    return {
-      content:content,
-      author:author,
-      id:randomNumberForPhraseChoice
-    };
+
+    return PHRASES[randomNumberForPhraseChoice];
   }
 
   function generateRandomColor() {
